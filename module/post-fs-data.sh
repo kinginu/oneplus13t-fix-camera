4eb0a2eafe4242ea4bced115cb4d99a898249c6b
kinginu <87055402+kinginu@users.noreply.github.com>
2025-06-13 23:20:08 +0900

    [add] enhance camera configuration by adding new DNG files, Owl_Algo models, and additional settings for improved functionality

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -10,19 +10,29 @@ set_perm_recursive $MODPATH 0 0 0755 0644
 
 # Mounting /odm/etc/camera/
 mount --bind $MODDIR/odm/etc/camera/config /odm/etc/camera/config
+mount --bind $MODDIR/odm/etc/camera/dng /odm/etc/camera/dng
+# mount --bind $MODDIR/odm/etc/camera/facerestore /odm/etc/camera/facerestore
+mount --bind $MODDIR/odm/etc/camera/omoji /odm/etc/camera/omoji
+mount --bind $MODDIR/odm/etc/camera/Owl_Algo_model /odm/etc/camera/Owl_Algo_model
 mount --bind $MODDIR/odm/etc/camera/CameraHWConfiguration.config /odm/etc/camera/CameraHWConfiguration.config
 mount --bind $MODDIR/odm/etc/camera/camera_engmode.xml /odm/etc/camera/camera_engmode.xml
 mount --bind $MODDIR/odm/etc/camera/eis_camera.vcfg /odm/etc/camera/eis_camera.vcfg
+mount --bind $MODDIR/odm/etc/camera/fwk_config.json /odm/etc/camera/fwk_config.json
 mount --bind $MODDIR/odm/etc/camera/mvg_sat_config.json /odm/etc/camera/mvg_sat_config.json
 mount --bind $MODDIR/odm/etc/camera/oplus_eis_camera.vcfg /odm/etc/camera/oplus_eis_camera.vcfg
-mount --bind $MODDIR/odm/etc/camera/settings_fdc.txt /odm/etc/camera/settings_fdc.txt
 mount --bind $MODDIR/odm/etc/camera/settings_fdc_lite.txt /odm/etc/camera/settings_fdc_lite.txt
+mount --bind $MODDIR/odm/etc/camera/settings_fdc.txt /odm/etc/camera/settings_fdc.txt
+mount --bind $MODDIR/odm/etc/camera/vainr_cfg.json /odm/etc/camera/vainr_cfg.json
+
 
 # Mounting /odm/etc/power_profile
 mount --bind $MODDIR/odm/etc/power_profile /odm/etc/power_profile
 
 # Mounting /odm/firmware/fastchg
 mount --bind $MODDIR/odm/firmware/fastchg /odm/firmware/fastchg
+
+# Mounting /odm/lib
+# mount --bind $MODDIR/odm/lib/
 
 # Mounting /odm/lib64
 mount --bind $MODDIR/odm/lib64/vendor.oplus.hardware.eid-V1-ndk.so /odm/lib64/vendor.oplus.hardware.esim-V1-ndk.so
